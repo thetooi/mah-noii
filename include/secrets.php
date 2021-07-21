606a25592de6b92183bcf5e0e466566c3db69564
Phisarn Budwong <mythetooi@gmail.com>
2021-07-21 12:49:39 +0700

    Update secrets.php

--- include/secrets.php
+++ include/secrets.php
@@ -1,10 +1,10 @@
-<?
+<?php
 if(!defined('IN_TRACKER'))
   die('Hacking attempt!');
 
 $mysql_host = "localhost";
-$mysql_user = "user";
-$mysql_pass = "pass";
+$mysql_user = "mnsql";
+$mysql_pass = "?Bbtw856";
 $mysql_db = "tracker";
 
 ?>
